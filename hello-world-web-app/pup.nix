5f6c1e879d9badf85dac574ff4bcc5ac71744a1a
inevitable360 <pvidalx@gmail.com>
2026-04-08 16:45:28 +0100

    PUP Wizard Hello World

---
+++ hello-world-web-app/pup.nix
@@ -0,0 +1,7 @@
+# PupWizard generated template for Hello World Web App
+{ pkgs ? import <nixpkgs> {} }:
+{
+  # Replace this with your real package build
+  appName = "hello-world-web-app";
+  appVersion = "0.0.1";
+}
--- hello-world/pup.nix
+++
@@ -1,2 +0,0 @@
-# TODO: replace with real DogeBox build instructions
-{}

c2bd12aaf16cc7a721490a7ed0708bc348dfdbe5
inevitable360 <pvidalx@gmail.com>
2026-04-16 20:51:40 +0100

    Fix quantum

--- quantum-explorer/pup.nix
+++ quantum-explorer/pup.nix
@@ -5,7 +5,7 @@ let
 
   qe_bin = pkgs.buildGoModule {
     pname = "quantum-explorer";
-    version = "0.1.0";
+    version = "0.1.24";
     src = ./service;
     vendorHash = null;
     go = pkgs.go_1_24;

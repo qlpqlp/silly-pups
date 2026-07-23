3bf8c13a83ab80ccc848fb26ddd827f83e97ec8f
inevitable360 <pvidalx@gmail.com>
2026-07-23 11:34:52 +0100

    DogeGo PUP Fix

--- DogeGo/pup.nix
+++ DogeGo/pup.nix
@@ -4,38 +4,87 @@
 # Builds from https://github.com/qlpqlp/dogego (modRoot = DogeGo).
 # Configure network / wallet / RPC in the DogeGo web UI after start.
 #
-# Use a custom buildPhase (same as https-proxy / pq-wallet / quantum-explorer).
-# Do NOT set top-level CGO_ENABLED: DogeBox nixpkgs puts CGO_ENABLED in `env`,
-# and overlapping drv-arg + env keys fails evaluation.
+# Avoid pkgs.buildGoModule: DogeBox nixpkgs sets env.CGO_ENABLED, and a
+# top-level CGO_ENABLED = "0" (legacy/injected) makes evaluation fail with
+# overlapping env vs derivation attributes.
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  dogego_bin = pkgs.buildGoModule {
+  src = pkgs.fetchgit {
+    url = "https://github.com/qlpqlp/dogego.git";
+    rev = "9d88c34dd3f8f64bc2c5c6afb58062b0da2adb5c";
+    hash = "sha256-r1OzX4f9whHdDEruH0/n+yW7kydgGl5S2cAWwaK2xuE=";
+  };
+
+  # Fixed-output vendor dir (same hash as former buildGoModule vendorHash).
+  goModules = pkgs.stdenv.mkDerivation {
+    name = "dogego-go-modules";
+    inherit src;
+    nativeBuildInputs = [
+      pkgs.go_1_24
+      pkgs.git
+      pkgs.cacert
+    ];
+    impureEnvVars = pkgs.lib.fetchers.proxyImpureEnvVars ++ [
+      "GIT_PROXY_COMMAND"
+      "SOCKS_SERVER"
+      "GOPROXY"
+    ];
+    configurePhase = ''
+      runHook preConfigure
+      export GOCACHE=$TMPDIR/go-cache
+      export GOPATH=$TMPDIR/go
+      cd DogeGo
+      runHook postConfigure
+    '';
+    buildPhase = ''
+      runHook preBuild
+      export GIT_SSL_CAINFO=$NIX_SSL_CERT_FILE
+      go mod vendor
+      mkdir -p vendor
+      runHook postBuild
+    '';
+    installPhase = ''
+      runHook preInstall
+      cp -r --reflink=auto vendor $out
+      runHook postInstall
+    '';
+    dontFixup = true;
+    outputHashMode = "recursive";
+    outputHash = "sha256-xwHNyDyPMEXSY7A71/t/mGdgtoXxibiHghu8OvfVOYI=";
+  };
+
+  dogego_bin = pkgs.stdenv.mkDerivation {
     pname = "dogego";
     version = "0.1.0";
+    inherit src;
 
-    go = pkgs.go_1_24;
+    nativeBuildInputs = [ pkgs.go_1_24 ];
 
-    src = pkgs.fetchgit {
-      url = "https://github.com/qlpqlp/dogego.git";
-      rev = "9d88c34dd3f8f64bc2c5c6afb58062b0da2adb5c";
-      hash = "sha256-r1OzX4f9whHdDEruH0/n+yW7kydgGl5S2cAWwaK2xuE=";
-    };
-
-    modRoot = "DogeGo";
-    vendorHash = "sha256-xwHNyDyPMEXSY7A71/t/mGdgtoXxibiHghu8OvfVOYI=";
-
-    doCheck = false;
+    dontConfigure = true;
 
     buildPhase = ''
+      runHook preBuild
       export GOCACHE=$TMPDIR/go-cache
+      export GOPATH=$TMPDIR/go
+      export GO111MODULE=on
+      export GOTOOLCHAIN=local
       export CGO_ENABLED=0
-      go build -trimpath -ldflags="-s -w" -o dogego ./cmd/dogego
+      export GOPROXY=off
+      export GOSUMDB=off
+      cd DogeGo
+      rm -rf vendor
+      cp -r --reflink=auto ${goModules} vendor
+      go build -mod=vendor -trimpath -ldflags="-s -w" -o dogego ./cmd/dogego
+      runHook postBuild
     '';
 
     installPhase = ''
+      runHook preInstall
       mkdir -p $out/bin
+      # buildPhase cds into DogeGo and leaves cwd there
       cp dogego $out/bin/dogego
+      runHook postInstall
     '';
   };
 

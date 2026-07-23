9394ac7a956f2f2da1965acec882438db325fbaf
inevitable360 <pvidalx@gmail.com>
2026-07-23 11:26:23 +0100

    DogeGo PUP Fix

--- DogeGo/pup.nix
+++ DogeGo/pup.nix
@@ -1,8 +1,12 @@
 # DogeGo PUP for DogeBox / silly-pups.
 # Service attr name MUST match manifest container.services[0].name ("dogego").
 #
-# Builds the Go node from https://github.com/qlpqlp/dogego (modRoot = DogeGo).
+# Builds from https://github.com/qlpqlp/dogego (modRoot = DogeGo).
 # Configure network / wallet / RPC in the DogeGo web UI after start.
+#
+# Use a custom buildPhase (same as https-proxy / pq-wallet / quantum-explorer).
+# Do NOT set top-level CGO_ENABLED: DogeBox nixpkgs puts CGO_ENABLED in `env`,
+# and overlapping drv-arg + env keys fails evaluation.
 { pkgs ? import <nixpkgs> {} }:
 
 let
@@ -10,12 +14,10 @@ let
     pname = "dogego";
     version = "0.1.0";
 
-    # Matches other silly-pups Go pups (gigawallet, pq-wallet, …).
     go = pkgs.go_1_24;
 
     src = pkgs.fetchgit {
       url = "https://github.com/qlpqlp/dogego.git";
-      # Pin a full commit SHA (not a moving tag).
       rev = "9d88c34dd3f8f64bc2c5c6afb58062b0da2adb5c";
       hash = "sha256-r1OzX4f9whHdDEruH0/n+yW7kydgGl5S2cAWwaK2xuE=";
     };
@@ -23,16 +25,18 @@ let
     modRoot = "DogeGo";
     vendorHash = "sha256-xwHNyDyPMEXSY7A71/t/mGdgtoXxibiHghu8OvfVOYI=";
 
-    # Do not set CGO_ENABLED here: DogeBox nixpkgs already puts it in env,
-    # and a top-level CGO_ENABLED overlaps (install fails).
     doCheck = false;
 
-    subPackages = [ "cmd/dogego" ];
+    buildPhase = ''
+      export GOCACHE=$TMPDIR/go-cache
+      export CGO_ENABLED=0
+      go build -trimpath -ldflags="-s -w" -o dogego ./cmd/dogego
+    '';
 
-    ldflags = [
-      "-s"
-      "-w"
-    ];
+    installPhase = ''
+      mkdir -p $out/bin
+      cp dogego $out/bin/dogego
+    '';
   };
 
   dogego = pkgs.writeShellScriptBin "run.sh" ''
